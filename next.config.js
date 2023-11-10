81968be1591c98fa88f06cafe1d9d9d2d4a6045e
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-11-10 23:37:02 +0600

    online education service

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "images.pexels.com",
+            "upload.wikimedia.org",
+            "media.licdn.com"
+        ],
+    },
+};
+
+module.exports = nextConfig
